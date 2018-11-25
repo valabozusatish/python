adb15ecea158fb4c587a87e6e75c572a31f8f09c
satish <satish.v.s@ncodeit.com>
2018-11-25 16:00:17 +0530

    ex1

--- ex1.py
+++ ex1.py
@@ -3,9 +3,5 @@ print "Hello Again"
 print "I like typing this."
 print "This is fun."
 print 'yay! printing.'
-
-
-
-print "I like typing this."
-print "This is fun."
-print 'yay! printing.'
+print "I'd much rather you 'not'."
+print 'I "said do not touch this.'
